cd8ea3ab234767d8b45b8fb6955d6f0e77afba46
vastja <vastja@gmail.com>
2024-06-10 21:33:07 +0200

    Comparison of two files (#2)
    * Parsing tag and amount from csv file
    
    * base and current expenses parsing
    
    * Comparison
    
    * Printing difference to console

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,118 @@
+use rust_decimal_macros::dec;
+use std::path::Path;
 use std::str::Split;
 use std::{env, fs};
-use std::path::Path;
+use rust_decimal::Decimal;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
-    if args.len() != 2 { 
-        println!("Usage: fat_piggy_bank input-csv-file-path")
+    if args.len() != 3 { 
+        println!("Usage: fat_piggy_bank base-expense-file current-expense-file")
     }
-    let path = Path::new(&args[1]);
+
+    let baseline_expenses : Vec<CostItem> = load_expenses(&args[1]);
+    let current_expenses : Vec<CostItem> = load_expenses(&args[2]);
+
+    let comparison : Vec<CostItem> = compare_expenses(baseline_expenses, current_expenses);
+    for expense_comparison in comparison {
+        println!("Difference for '{}' was {} CZK.", expense_comparison.tag, expense_comparison.amount)
+    }
+}
+
+fn load_expenses(path: &str) -> Vec<CostItem> {
+    if !Path::exists(Path::new(path)) {
+        panic!("File {} does not exist.", path)
+    } 
 
     let contents : String = fs::read_to_string(path).expect("Could not read provided file.");
+    let lines: Vec<&str> = contents.lines().skip(1).collect();
+    get_items(lines, "Kategorie", "Částka v měně účtu")
+}
 
-    let lines: Vec<&str> = contents.lines().collect();
+#[derive(Debug, PartialEq, Clone)]
+struct CostItem {
+    tag : String,
+    amount : Decimal
+}
 
-    let mut header = lines[1].split(",");
-    let tag_index : usize = header.position(|x| x == "Kategorie").unwrap();
-    let amount_index : usize = header.position(|x| x == "Částka v měně účtu").unwrap();
-
-    for line in lines.iter().skip(2) {
-        let mut parts: Split<&str> = line.split(",");
-        println!("Tag: {}; amount: {}", parts.nth(tag_index).unwrap(), parts.nth(amount_index).unwrap())
+impl CostItem {
+    pub fn new(tag: &str, amount: Decimal) -> Self {
+        CostItem {
+            tag: tag.to_string(),
+            amount : amount
+        }       
     } 
+}
+
+fn get_items(lines : Vec<&str>, tag_col_name: &str, amount_col_name: &str) -> Vec<CostItem> {
+    let mut header = lines[0].split(",");
+    let tag_index : usize = header.position(|x| x.trim() == tag_col_name).unwrap();
+    let amount_index : usize = header.position(|x| x.trim() == amount_col_name).unwrap();
+
+    let mut items = Vec::new();
+    for line in lines.iter().skip(1) {
+        let mut parts: Split<&str> = line.split(",");
+        let item = CostItem {
+            tag:  parts.nth(tag_index).unwrap().trim().to_string(),
+            amount: parts.nth(amount_index).unwrap().trim().parse().unwrap()
+        };
+        items.push(item);
+    } 
+    items
+}
+
+fn compare_expenses(baseline: Vec<CostItem>, current: Vec<CostItem>) -> Vec<CostItem> {
+    let mut comparison : Vec<CostItem> = vec![];
+    for expense in baseline.iter() {
+        let current_expense : Decimal = match current.iter().find(|x| x.tag == expense.tag) {
+            Some(item) => item.amount,
+            None => dec!(0)
+        };
+        comparison.push(CostItem::new(&expense.tag, current_expense - expense.amount));
+    }
+
+    let new_expenses = current.iter().filter(|x| !baseline.iter().any(|y| y.tag == x.tag));
+    for expense in new_expenses {
+        comparison.push(expense.clone());
+    }
+
+    comparison.sort_by(|x, y|  y.amount.cmp(&x.amount));
+    comparison
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rust_decimal_macros::dec;
+    
+    #[test]
+    fn parse_provided_expense_data() {
+        let data = [ "tag, amount",  "food, 75"].into_iter().collect();
+
+        let items : Vec<CostItem> = get_items(data, "tag", "amount");
+
+        assert_eq!(items.contains(&CostItem { tag : String::from("food"), amount : dec!(75) }), true);
+    }
+
+    #[test] 
+    fn expenses_comparison_order_from_worst_to_best() {
+        let base = [
+                CostItem::new("food", dec!(75)),
+                CostItem::new("home", dec!(1055.6)),
+                CostItem::new("other", dec!(100))
+            ].into_iter().collect();
+
+        let current = [
+                CostItem::new("food", dec!(140.5)),
+                CostItem::new("home", dec!(1000)),
+                CostItem::new("sport", dec!(125))
+            ].into_iter().collect();
+
+        let result : Vec<CostItem> = compare_expenses(base, current);
+
+        assert_eq!(result[0], CostItem::new("sport", dec!(125)));
+        assert_eq!(result[1], CostItem::new("food", dec!(65.5)));
+        assert_eq!(result[2], CostItem::new("home", dec!(-55.6)));
+        assert_eq!(result[3], CostItem::new("other", dec!(-100)));
+    }
 }
