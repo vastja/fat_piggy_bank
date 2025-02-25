9c5800ff84d97f02773584b55ebb449ce3e56455
vastja <vastja@gmail.com>
2025-02-25 21:03:39 +0100

    Table with static data

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,65 @@
+import { format } from 'path';
 import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
     const [text, setText] = useState<string>("");
-
-
-    useEffect(() => {
-        const fetchText = async () => {
-            const response = await fetch('/api');
-            const data = await response.text();
-            console.info(data);
-            setText(data);
-        };
-        fetchText();
-    }, []);
+    const expensesService: ExpensesService = staticExpensesService;
+    const expenses: Expense[] = expensesService.fetchExpenses();
+    // useEffect(() => {
+    // const response = await fetch('/api');
+    // const data = await response.text();
+    // console.info(data);
+    // setText(data);
+    // }, []);
 
     return (
         <div className="App">
             <header className="App-header">
-                <div>
-                    {text}
-                </div>
+                <table className="table-auto border-collapse w-full border">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Tag</th>
+                            <th>Amount</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {expenses.map((expense) => (
+                            <tr key={expense.id}>
+                                <td>{formatDate(expense.date)}</td>
+                                <td>{expense.tag}</td>
+                                <td>{expense.amount}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </header>
         </div>
     );
+}
+
+interface ExpensesService {
+    fetchExpenses(): Expense[]
+}
+
+interface Expense {
+    id: number,
+    tag: string,
+    amount: number,
+    date: Date
+}
+
+const staticExpensesService: ExpensesService = {
+    fetchExpenses(): Expense[] {
+        return [
+            { id: 0, tag: "Grocery", amount: 100, date: new Date("2024-5-17") }
+        ]
+    }
+}
+
+function formatDate(date: Date): string {
+    return [date.getDay(), date.getMonth(), date.getFullYear()].join('-');
 }
 
 export default App;
